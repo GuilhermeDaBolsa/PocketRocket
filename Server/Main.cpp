82d7006861b3a183cbff0a86650b7d98009dbb30
Guilherme Paini <guihdpaini@gmail.com>
2022-11-18 21:07:38 -0300

    WIP: trying sockets

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,6 +1,4 @@
 #include "Crow_all.h"
-#include <vector>
-#include <string>
 
 #include "./controllers/UserController.h"
 
@@ -9,27 +7,37 @@ using namespace crow;
 using namespace controllers;
 
 int main() {
-    SimpleApp       app;
-    UserController  userController;
+
+    App<CORSHandler>    app;
+    UserController      userController;
+
+    auto& cors = app.get_middleware<CORSHandler>();
+
+    cors
+      .global()
+        .methods("POST"_method, "GET"_method)
+      .prefix("*")
+        .ignore();
 
     CROW_ROUTE(app, "/")([]() { return "Hello, world!"; });
     
     CROW_ROUTE(app, "/bomdia").methods("GET"_method)(&userController.Print);
     CROW_ROUTE(app, "/signUp").methods("POST"_method)(&userController.SignUp);
+
     CROW_WEBSOCKET_ROUTE(app, "/ws")
-        .onopen([&](crow::websocket::connection& conn){
-            //
-            cout << "close" << std::endl;
+        .onopen([&](crow::websocket::connection& conn) {
+            CROW_LOG_INFO << "websocket connection is open: ";
         })
         .onclose([&](crow::websocket::connection& conn, const std::string& reason){
-            // do_something();
-            cout << "close" << std::endl;
+            CROW_LOG_INFO << "websocket connection closed: " << reason;
         })
         .onmessage([&](crow::websocket::connection& /*conn*/, const std::string& data, bool is_binary){
-            cout << "bomdia" << std::endl;
+            CROW_LOG_INFO << data << "< message data";
         });
 
-    app.port(8080).multithreaded().run();
+    app.port(8080)
+        .multithreaded()
+        .run();
 
     return 0;
 }
